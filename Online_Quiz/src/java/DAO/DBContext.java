18ee59bf70f7c0927343a9cf59ca40d9f42ecba1
duongphhe151097 <duongphhe151097@fpt.edu.vn>
2022-05-19 13:33:47 +0700

    Add main-project

---
+++ Online_Quiz/src/java/DAO/DBContext.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author hongd
+ */
+public class DBContext {
+    public Connection connection;
+    public DBContext() {
+        try {
+            //Change the username password and url to connect your own database
+//            FileReader reader = new FileReader("config.properties");  
+//            Properties p = new Properties();
+//            p.load(reader);  
+            String username = "sa";
+            String password = "12345";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=Online_Quiz";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException ex ) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ Online_Quiz/src/java/DAO/TestConnection.java
@@ -0,0 +1,37 @@
+package DAO;
+
+
+import DAO.DBContext;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hongd
+ */
+public class TestConnection extends DBContext{
+    public void testConnection(){
+        String query = "Select * from Account";
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            if (rs != null) {
+                System.out.println("Success");
+            }
+        } catch (SQLException e) {
+        }
+//        System.out.println("Fail!");
+    }
+    
+    public static void main(String[] args) {
+        TestConnection ts = new TestConnection();
+        ts.testConnection();
+    }
+}
