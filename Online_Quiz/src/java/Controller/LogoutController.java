c036aaf712cc539a23cd29006c917b78b712d52d
thinhddhe151091 <thinhddhe151091@fpt.edu.vn>
2022-05-31 01:57:57 +0700

    edit home login

--- Online_Quiz/src/java/Controller/LogoutController.java
+++ Online_Quiz/src/java/Controller/LogoutController.java
@@ -5,14 +5,10 @@
  */
 package Controller;
 
-import DAO.AccountDAO;
-import DAO.DAO;
-import Model.Account;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,8 +18,8 @@ import javax.servlet.http.HttpSession;
  *
  * @author PREDATOR
  */
-@WebServlet(name = "LoginServlet", urlPatterns = {"/login"})
-public class LoginServlet extends HttpServlet {
+@WebServlet(name = "LogoutController", urlPatterns = {"/logout"})
+public class LogoutController extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -37,18 +33,9 @@ public class LoginServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        HttpSession session = request.getSession();
+        session.removeAttribute("acc");
+        response.sendRedirect("home");
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -63,8 +50,7 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.getRequestDispatcher("login.jsp").forward(request, response);
-
+        processRequest(request, response);
     }
 
     /**
@@ -78,18 +64,7 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
-        AccountDAO dao = new AccountDAO();
-        Account a = dao.login(email, password);
-        if (a == null || a.getStatus() == 0) {
-            request.setAttribute("mess", "Wrong email or password!!!");
-            request.getRequestDispatcher("login.jsp").forward(request, response);
-        } else {
-            HttpSession Session = request.getSession();
-            Session.setAttribute("acc", a);
-            response.sendRedirect("home");
-        }
+        processRequest(request, response);
     }
 
     /**
